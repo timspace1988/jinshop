49535cd20f8f6b8328eb9aa3900ab7838ffb1e88
JIA <timspace1988@hotmail.com>
2020-07-30 13:03:32 +0000

    basic layouts header

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class=container>
+        <!-- branding image -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+            JiaShop
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- left side of navbar -->
+            <ul class="navbar-nav mr-auto">
+            </ul>
+
+            <!-- right side of navbar -->
+            <ul class="navbar-nav navbar-right">
+                <!-- authentication links -->
+                <li class="nav-item"><a class="nav-link" href="#"></a>Sign in</li>
+                <li class="nav-item"><a class="nav-link" href="#"></a>Sign up</li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- csrf token -->
+    <title>@yield('title', 'jiashop') - a creative online shop</title>
+    <!-- style -->
+    <link rel="stylesheet" href="{{ mix(css/app.css) }}">
+</head>
+<body>
+    <div id="app" class="{{ route_class()}}-page ">
+        @include('layout.header')
+        <div class="container">
+            @yield('contant')
+        </div>
+        @include('layout.footer')
+    </div>
+    <!-- js script -->
+    <script src="{{ mix('js/app.js) }}"></script>
+</body>
+</html>
