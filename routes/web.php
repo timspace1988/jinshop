cabf96dd361b4b18f048bb06f247caa8a7433774
JIA <timspace1988@hotmail.com>
2020-08-02 04:44:21 +0000

    email verification

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 //Route::get('/home', 'HomeController@index')->name('home');
