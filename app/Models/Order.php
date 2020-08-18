4477081f1d22c65833e58561f7df69cc9d2f2905
JIA <timspace1988@hotmail.com>
2020-08-18 10:17:00 +0000

    order model

---
+++ app/Models/Order.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    const REFUND_STATUS_PENDING = 'pending';
+    const REFUND_STATUS_APPLIED = 'applied';
+    const REFUND_STATUS_PROCESSING = 'processing';
+    const REFUND_STATUS_SUCCESS = 'success';
+    const REFUND_STATUS_FAILED = 'failed';
+
+    const SHIP_STATUS_PENDING = 'pending';
+    const SHIP_STATUS_DELIVERED = 'delivered';
+    const SHIP_STATUS_RECEIVED = 'received';
+
+    public static $refundStatusMap = [
+        self::REFUND_STATUS_PENDING => 'pending',
+        self::REFUND_STATUS_APPLIED => 'applied',
+        self::REFUND_STATUS_PROCESSING => 'processing',
+        self::REFUND_STATUS_SUCCESS => 'success',
+        self::REFUND_STATUS_FAILED => 'failed',
+    ];
+
+    public static $shipStatusMap = [
+        self::SHIP_STATUS_PENDING => 'pending',
+        self::SHIP_STATUS_DELIVERED => 'delivered',
+        self::SHIP_STATUS_RECEIVED => 'received',
+    ];
+
+    protected $fillable = [
+        'no',
+        'address',
+        'total_amount',
+        'remark',
+        'paid_at',
+        'payment_method',
+        'payment_no',
+        'refund_status',
+        'refund_no',
+        'closed',
+        'reviewed',
+        'ship_status',
+        'ship_data',
+        'extra',
+    ];
+
+    protected $casts = [
+        'closed' => 'boolean',
+        'reviewed' => 'boolean',
+        'address' => 'json',
+        'ship_data' => 'json',
+        'extra' => 'json',
+    ];
+
+    protected $dates = [
+        'paid_at',
+    ];
+
+    protected static function boot(){
+        parent::boot();
+        //listening on model creating event, 
+        static::creating(function($model){
+            //this function will be executed before data being written to database
+            if(!$model->no){
+                //if this model's 'no' is empty, call findAvaiableNo() to generate the order no
+                $model->no = static::findAvaiableNo();
+                //if generation failed, terminate creating order 
+                if(!$model->no){
+                    return false;
+                }
+            }
+        });
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function items(){
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public static function findAvaiableNo(){
+        $prefix = date('YmdHis');
+        for($i = 0; $i<10; $i++){
+            //generate order no by connecting the prefix with a random 6-digit number
+            $no = $prefix.str_pad(random_int(0,999999), 6, '0', STR_PAD_LEFT);//str_pad() will convert 126 to 000126
+            //call this class's static function query() to check if this order no already exists, if not, return the 'no' just generated
+            if(!static::query()->where('no', $no)->exists()){
+                return $no;
+            }
+        }
+        /*
+        if the generated no is same with some existing order no, it means they have a conflict, we cannot use this generated no, so we keep generating for 10 times until we got the un-conflict one,
+        however, if we still cannot get a unique no, we give out a warning about failed generating order no
+        note: the above code will ensure the randomly generated order no is unique, because the order no important (usually we can get one within 10 times)
+        */
+        \Log::warning('Failed to generate a unique order no');
+        return false;
+    }
+}
