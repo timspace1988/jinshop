2067f04761374312bf96140631350efb4b64dc98
JIA <timspace1988@hotmail.com>
2020-08-22 04:32:26 +0000

    install payment extension package yansongda/pay

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Symfony\Component\ErrorHandler\Debug;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,34 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //inject an singleton object named 'alipay' into container
+        $this->app->singleton('alipay', function(){
+            $config = config('pay.alipay');
+            //check the environment to decide if enable alipay's developing mode and set different level of logs
+            if(app()->environment() !== 'production'){
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            }else{
+                $config['log'] = Logger::WARNING;
+            }
+
+            //call Yansongda\Pay to create a alipay object, then we can use app('alipay') to create an aplipay instance
+            return Pay::alipay($config);
+        });
+
+        //inject an singleton object named 'wechat_pay' into container
+        $this->app->singleton('wechat_pay', function(){
+            $config = config('pay.wechat');
+            //as wechat does not come with a developing mode, we just set the log level depending on different environment
+            if(app()->environment() !== 'production'){
+                $config['log']['level'] = Logger::DEBUG;
+            }else{
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            //call Yansongda\Pay to create a wechat_pay object
+            return Pay::wechat($config);
+        });
     }
 
     /**
