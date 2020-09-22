a26ce69f18ef0166ce7bb3ce9ed11e2841d0befe
JIA <timspace1988@hotmail.com>
2020-09-22 00:22:42 +0000

    heroku debug

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -18,7 +18,7 @@
                             <th class="text-right item-amount">Subtotal</th>
                         </tr>
                     </thead>
-                    <!-- @foreach($order->items as $index => $item)
+                    @foreach($order->items as $index => $item)
                         <tr>
                             <td class="product-info">
                                 <div class="preview">
@@ -37,7 +37,7 @@
                             <td class="sku-amount text-center vertical-middle">{{ $item->amount }}</td>
                             <td class="item-amount text-right vertical-middle">${{ number_format($item->price * $item->amount, 2, '.', '') }}</td>
                         </tr>
-                    @endforeach -->
+                    @endforeach
                     <tr><td colspan="4"></td></tr>
                 </table>
                 <div class="order-bottom">
@@ -51,7 +51,7 @@
                             <div class="line-value">{{ \App\Models\Order::$shipStatusMap[$order->ship_status] }}</div>
                         </div>
                         <!-- If the order has got shipment data, display it -->
-                        <!-- @if($order->ship_data)
+                        @if($order->ship_data)
                             <div class="line">
                                 <div class="line-label">Courier :&nbsp;</div>
                                 <div class="line-value">{{ $order->ship_data['express_company'] }}</div>
@@ -60,9 +60,9 @@
                                 <div class="line-label">Shipment no :&nbsp;</div>
                                 <div class="line-value">{{ $order->ship_data['express_no'] }}</div>
                             </div>
-                        @endif -->
+                        @endif
                         <!-- If order is paid and refund status is not peding, display refund info -->
-                        <!-- @if($order->paid_at && $order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
+                        @if($order->paid_at && $order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
                             <div class="line">
                                 <div class="line-label">Refund :&nbsp;</div>
                                 <div class="line-value">{{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}</div>
@@ -71,64 +71,9 @@
                                 <div class="line-label">Reason :&nbsp;</div>
                                 <div class="line-value">{{ $order->extra['refund_reason'] }}</div>
                             </div>
-                        @endif -->
+                        @endif
                     </div>
-                    <div class="order-summary text-right">
-                        <!-- display coupon info -->
-                        <!-- @if($order->couponCode)
-                        <div class="text-primary">
-                            <span>Discount with coupon applied: </span>
-                            <div class="value">{{ $order->couponCode->description }}</div>
-                        </div>
-                        @endif -->
-                        <!-- end of display coupon info -->
-                        <div class="total-amount">
-                            <span>Total amount: </span>
-                            <div class="value">${{ $order->total_amount }}</div>
-                        </div>
-                        <div>
-                            <span>Order status: </span>
-                            <div class="value">
-                                <!-- @if($order->paid_at)
-                                    @if($order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
-                                        Paid
-                                    @else
-                                        {{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}
-                                    @endif
-                                @elseif($order->closed)
-                                    Closed
-                                @else
-                                    Not paid
-                                @endif -->
-                            </div>
-                        </div>
-                        <!-- if refund aplication is declined, display the disagree reason -->
-                        <!-- @if(isset($order->extra['refund_disagree_reason']))
-                            <div>
-                                <span>Refund declined: </span>
-                                <div class="value">{{ $order->extra['refund_disagree_reason'] }}</div>
-                            </div>
-                        @endif -->
-                        <!-- pay button -->
-                        <!-- @if(!$order->paid_at && !$order->closed)
-                        <div class="payment_buttons">
-                            <a href="{{ route('payment.alipay', ['order' => $order->id]) }}" class="btn btn-primary btn-sm">AliPay/支付宝</a>
-                        </div>
-                        @endif -->
-                        <!-- finish pay button -->
-
-                        <!-- if order status is in-delivery(this project mark this status with delivered), display the received button -->
-                        
-                        <!-- if order is paid and refund status is pending and is not crowdfunding order, then display apply for refund button -->
-                        <!-- @if($order->type !== \App\Models\Order::TYPE_CROWDFUNDING && 
-                            $order->paid_at && 
-                            $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
-                            
-                            <div class="refund-button">
-                                <button class="btn btn-sm btn-danger" id="btn-apply-refund">Apply for refund</button>
-                            </div>
-                        @endif -->
-                    </div>
+                    
                 </div>
             </div>
         </div>
