80bc4e6433d2a261c6d7a7940e9d28c912a2c268
JIA <timspace1988@hotmail.com>
2020-09-28 22:13:13 +0000

    Installment list page for user

---
+++ resources/views/installments/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('title', 'Installment list')
+
+@section('content')
+    <div class="row">
+        <div class="col-10 offset-1">
+            <div class="card">
+                <div class="card-header text-center"><h2>Installment list</h2>
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Installment no</th>
+                                    <th>Total amount</th>
+                                    <th>Phases</th>
+                                    <th>Fee rate</th>
+                                    <th>Status</th>
+                                    <th>Operations</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($installments as $installment)
+                                    <tr>
+                                        <td>{{ $installment->no }}</td>
+                                        <td>$ {{ $installment->total_amount }}</td>
+                                        <td>{{ $installment->count }}</td>
+                                        <td>{{ $installment->fee_rate }} %</td>
+                                        <td>{{ \App\Models\Installment::$statusMap[$installment->status] }}</td>
+                                        <td><a href="" class="btn btn-primary btn-sm">View</a></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="float-right">{{ $installments->render() }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,7 @@ Route::group(['middleware' => ['auth', 'verified']], function(){
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
+    Route::get('installments', 'InstallmentsController@index')->name('installments.index');
 });
 
 //This cannot go through auth middleware, because alipay sends this request to our back-end server instead of redirecting to a page, it doesn't contains any auth info 
