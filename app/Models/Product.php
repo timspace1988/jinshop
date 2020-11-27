925d48326909164c57655111c893df26ce046197
JIA <timspace1988@hotmail.com>
2020-11-27 04:01:57 +0000

    heroku debug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -111,11 +111,8 @@ class Product extends Model
     //scopeXxxYyy will allows us to create a method for 'linked execution', scopeByIds($query, $param) will be used in format: Product::query()->byIds($param)
     public function scopeByIds($query, $ids){
         if(getenv('IS_IN_HEROKU')){
-            //in postgres, ANY(string_to_array()) is equivalent to FIND_IN_SET() in mysql
-            return $query->whereIn('id', $ids)->orderByRaw(sprintf("id::text = ANY(string_to_array('%s', ','))", join(',', $ids)));
-            //dd($ids);
-
-            //return $query->whereIn('id', $ids)->orderByRaw(sprintf("array_position(string_to_array('%s', ','), id::text)", join(',', $ids)));
+            //in postgres, order by array_position(array['1', '2', '3'], id::text) is equivalent to order by FIND_IN_SET(id, '1, 2, 3') in mysql
+            return $query->whereIn('id', $ids)->orderByRaw(sprintf("array_position(string_to_array('%s', ','), id::text)", join(',', $ids)));
            
         }else{
             return $query->whereIn('id', $ids)->orderByRaw(sprintf(     "FIND_IN_SET(id, '%s')", join(',', $ids)     ));
